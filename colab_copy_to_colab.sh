d06fb83d3d06930270be1a5f27dc09bc66b38196
Your Name <your_email@example.com>
2024-06-08 22:14:17 +0200

    colab copy

---
+++ colab_copy_to_colab.sh
@@ -0,0 +1,3 @@
+# copy directory /content/drive/MyDrive/ComfyUI to /content/ and overwrite if it already exists
+echo "Copying ComfyUI from Google Drive to /content/..."
+cp -r /content/drive/MyDrive/ComfyUI /content/
---
+++ colab_copy_to_drive.sh
@@ -0,0 +1,3 @@
+# copy directory /content/ to /content/drive/MyDrive/ComfyUI and overwrite if it already exists
+echo "Copying ComfyUI from Google Drive to /content/..."
+cp -r /content/ComfyUI /content/drive/MyDrive/
